86161eb1040b737c9e4840bdaa9c6a880cbd9f43
IonutGaina <gainaionutz@gmail.com>
2022-05-12 01:38:32 +0300

    task 4 completed

--- task-4/spiral.asm
+++ task-4/spiral.asm
@@ -21,7 +21,7 @@ spiral:
     mov eax, [ebp + 8]  ; N (size of key line)
     mov ebx, [ebp + 12] ; plain (address of first element in string)
     mov ecx, [ebp + 16] ; key (address of first element in matrix)
-    mov edx, [ebp + 20] ; enc_string (address of first element in string)
+    mov edi, [ebp + 20] ; enc_string (address of first element in string)
     ;; DO NOT MODIFY
     ;; TODO: Implement spiral encryption
     ;; FREESTYLE STARTS HERE
@@ -31,17 +31,28 @@ spiral:
     mov dword[n], eax
     mov dword[plain], ebx
     mov dword[key], ecx
-    mov dword[enc_string], edx
+    mov dword[enc_string], edi
 
-    xor ecx, ecx
-    xor edx, edx
     PRINTF32 `eax: %d\n\x0`, eax
+    push ebx
+    xor eax, eax
+    xor edx, edx
+    xor ebx, ebx
+    
 
     mov eax, [n]
     mov ebx, 2
     div ebx
 
-    ; esi, edi, eax
+    pop ebx
+    push edx
+
+    ; eax - pana la cat
+    ; esi - iteratorul
+    ; edx - restul n/2
+    ; ebx - plain
+    ; ecx - key
+    ; edi - enc_string
 
     ; eax = n/2 , edx = rest
     mov esi, 0
@@ -57,20 +68,159 @@ while_matrix:
 
     sub eax, esi 
     dec eax
-    PRINTF32 `latura %d\n\x0`, esi
+    ; START
+    push esi
+    ;PRINTF32 `latura %d\n\x0`, esi
 upper_side:
 
-    PRINTF32 `%d \x0`, esi
+    ;PRINTF32 `%d \x0`, esi
 
-    ; am creat latura de sus
-    ; acum trebuie sa criptez
-    ; TO DO
+    xor edx, edx
+    mov dl, byte[ebx]       ; take letter from plain
+    inc ebx
+
+    push eax
+
+    mov eax, dword[ecx]     ; take integer from key[][]
+    ;PRINTF32 `%d \x0`, eax
+    add ecx, 4
+    
+    add edx, eax            ; encrypt
+    mov byte[edi], dl       ; put crypted letter in enc_ecrypt
+    inc edi   
+    pop eax
+
 
     inc esi
     cmp esi, eax
     jl upper_side
+    ; END 
 
-    PRINTF32 `\n\n\x0`, esi
+    pop esi
+    ; START
+    push esi
+    ;PRINTF32 `latura %d\n\x0`, esi
+
+right_side:
+
+    ;PRINTF32 `%d \x0`, esi
+
+    xor edx, edx
+    mov dl, byte[ebx]       ; take letter from plain
+    inc ebx
+    ;PRINTF32 `plain: %c\n\x0`, edx
+
+    push eax
+    push ebx
+    mov ebx, dword[n]
+
+    mov eax, dword[ecx]     ; take integer from key[][]
+    ;PRINTF32 `%d \x0`, eax
+
+
+    ; + 4 * ebx
+    add ecx, ebx
+    add ecx, ebx
+    add ecx, ebx
+    add ecx, ebx
+    
+    pop ebx
+    
+    add edx, eax            ; encrypt
+    mov byte[edi], dl       ; put crypted letter in enc_ecrypt
+    inc edi   
+    pop eax
+
+
+    inc esi
+    cmp esi, eax
+    jl right_side
+    ; END
+    pop esi
+    ; START
+    push esi
+    ;PRINTF32 `latura %d\n\x0`, esi
+
+bottom_side:
+
+    ;PRINTF32 `%d \x0`, esi
+
+    xor edx, edx
+    mov dl, byte[ebx]       ; take letter from plain
+    inc ebx
+    ;PRINTF32 `plain: %c\n\x0`, edx
+
+    push eax
+
+
+    mov eax, dword[ecx]     ; take integer from key[][]
+    ;PRINTF32 `%d \x0`, eax
+    sub ecx, 4
+
+    add edx, eax            ; encrypt
+    mov byte[edi], dl       ; put crypted letter in enc_ecrypt
+    inc edi   
+    pop eax
+
+
+    inc esi
+    cmp esi, eax
+    jl bottom_side
+    ; END
+    pop esi
+    ; START
+    push esi
+    ;PRINTF32 `latura %d\n\x0`, esi
+
+left_side:
+
+    ;PRINTF32 `%d \x0`, esi
+
+    xor edx, edx
+    mov dl, byte[ebx]       ; take letter from plain
+    inc ebx
+
+    push eax
+    push ebx
+    mov ebx, dword[n]
+    xor eax, eax
+    mov eax, dword[ecx]     ; take integer from key[][]
+
+    ;PRINTF32 `plain: %d\n\x0`, eax
+
+
+    ; - 4 * ebx
+    sub ecx, ebx
+    sub ecx, ebx
+    sub ecx, ebx
+    sub ecx, ebx
+
+    pop ebx
+    
+    add edx, eax            ; encrypt
+    mov byte[edi], dl       ; put crypted letter in enc_ecrypt
+    inc edi   
+    pop eax
+
+
+    inc esi
+    cmp esi, eax
+    jl left_side
+    ; END
+    push ebx
+    mov ebx, dword[n]
+
+    ; I take it to the next square/quadrant ( + 4 * ebx + 4)
+    add ecx, ebx
+    add ecx, ebx
+    add ecx, ebx
+    add ecx, ebx
+    add ecx, 4
+
+    pop ebx
+    pop esi
+
+    ;PRINTF32 `\n\n\x0`, esi
     
     pop esi
     pop eax
@@ -80,7 +230,23 @@ upper_side:
 
 skip_matrix:
 
-    ; TO DO
+    pop edx
+
+    cmp edx, 0
+    je skip_odd
+    
+    xor edx, edx
+    mov dl, byte[ebx]       ; take letter from plain
+
+    mov eax, dword[ecx]     ; take integer from key[][]
+    ;PRINTF32 `%d \x0`, eax
+
+    
+    add edx, eax            ; encrypt
+    mov byte[edi], dl       ; put crypted letter in enc_ecrypt
+
+
+skip_odd:
     
     ;; FREESTYLE ENDS HERE
     ;; DO NOT MODIFY
